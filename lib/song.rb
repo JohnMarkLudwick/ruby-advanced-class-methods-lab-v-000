b412ede110b4f61983de8317c21443e0d2ad0117
JohnMark <johnmarkludwick@gmail.com>
2018-06-28 17:15:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,13 @@ def self.find_by_name(name)
 end
 
 def find_or_create_by_name(name)
-  
+   @@all.each do |song|
+    if song.name == name
+      return song
+    else
+      song.New
+    end
+  end  
   
 end
   
