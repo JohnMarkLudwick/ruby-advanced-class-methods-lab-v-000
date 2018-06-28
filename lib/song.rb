d8c507022c6d4ec94ed7d924a9fe3f7118f3a842
JohnMark <johnmarkludwick@gmail.com>
2018-06-28 17:52:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,13 +63,7 @@ def self.alphabetical
     self.all.sort_by { |x| x.name}
   end 
 
-def self.new_from_filename(mp3_formatted_file)
-    c = self.new 
-    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
-    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
-    c
-  end 
-  
+
   
 end
   
